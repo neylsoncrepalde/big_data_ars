b996d405a6691d26bb86c827cc91efa0f84ce4ce
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-11-06 18:50:00 -0200

    up to blockmodel

--- sna_tweets.R
+++ sna_tweets.R
@@ -8,9 +8,11 @@
 # Se os pacotes necessários não estiverem instalados, faça a instalação
 if (! "rtweet" %in% installed.packages()) install.packages("rtweet")
 if (! "dplyr" %in% installed.packages()) install.packages("dplyr")
+if (! "tidyr" %in% installed.packages()) install.packages("tidyr")
 if (! "ggplot2" %in% installed.packages()) install.packages("ggplot2")
 if (! "igraph" %in% installed.packages()) install.packages("igraph")
 if (! "stringr" %in% installed.packages()) install.packages("stringr")
+if (! "sna" %in% installed.packages()) install.packages("sna")
 
 # Carrega os pacotes necessários
 library(rtweet)  # conexão com o twitter API
@@ -18,6 +20,7 @@ library(dplyr)   # manipulação de bancos de dados estilo tidy
 library(tidyr)   # manipulação de dados
 library(ggplot2) # Visualização de dados
 library(igraph)  # ARS - análises e visualização
+library(sna)     # Functions for social network analysis
 library(stringr) # Manipulação de texto
 
 # Se já tivermos os tweets salvos num arquivo csv, podemos importá-los para o R com o comando
@@ -90,7 +93,37 @@ ggplot(NULL, aes(indeg)) +
        subtitle = "Dados coletados do Twitter em 06/11/2018")
 
 
-## Rede de retweets ####
+# Plotando com grau de entrada no tamanho dos vértices e labels
+plot(g_citacoes, vertex.size = degree(g_citacoes, mode = "in", normalized = T)*200,
+     vertex.label.cex = degree(g_citacoes, mode = "in") / 300, 
+     #vertex.label = NA,
+     vertex.color = adjustcolor("orange", .7),
+     edge.arrow.size = .3, edge.color = adjustcolor("grey",.8),
+     layout = layoutgg)
 
 
+## Identificando comunidades
+# O igraph possui alguns algoritmos para detecção de comunidades em grafos.
+# Vamos experimentar os mais comuns
+grupos_walktrap = cluster_walktrap(g_citacoes)  # Rápido
+grupos_infomap = cluster_infomap(g_citacoes)    # Lento!!
+
+membership(grupos_walktrap)
+membership(grupos_infomap)
+
+# Plota com os grupos
+plot(g_citacoes, vertex.size = degree(g_citacoes, mode = "in", normalized = T)*200,
+     #vertex.label.cex = degree(g_citacoes, mode = "in") / 300, 
+     vertex.label = NA,
+     vertex.color = membership(grupos_walktrap),
+     edge.arrow.size = .3, edge.color = adjustcolor("grey",.8),
+     layout = layoutgg,
+     mark.groups = communities(grupos_walktrap))
+
+# Não é bom para redes muito grandes. Vamos tentar uma redução por equivalência estrutural usando
+# blockmodeling
+#ec = equiv.clust(as.matrix(get.adjacency(g_citacoes)))
+#blockmodel(g_citacoes, ec)
+#library(blockmodels)
+#blockmodels::BM_bernoulli
 
